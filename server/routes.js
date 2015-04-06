759a8b74142b6c4a8e6fa76acc0c9ca5cac83177
Etan <eberkowitz@salesforce.com>
2015-04-06 11:09:42 -0700

    first CRUD commit

--- server/routes.js
+++ server/routes.js
@@ -7,27 +7,40 @@ var render = require('../lib/render');
 var Models = require('../lib/models');
 var Tweet = require('./tweet');
 
+// Database
+
+var posts = [];
+
 /**
  * Define `Routes`.
  */
 
 var Routes = {};
 
-/**
- * Render index html page.
- */
+Routes.list = function *list() {
+  this.body = yield render('list', { posts: posts });
+}
 
-Routes.index = function *index() {
-  this.body = yield render('index');
-};
+Routes.add = function *add() {
+  this.body = yield render('new');
+}
 
-/**
- * Do foo.
- */
+Routes.show = function *show(id) {
+  var post = posts[id];
+  if (!post) this.throw(404, 'invalid post id');
+  this.body = yield render('show', { post: post });
+}
 
-Routes.doFoo = function *doFoo() {
-  this.body = 'OK';
-};
+Routes.create = function *create() {
+  var post = {
+  	title: this.request.body.title,
+  	body: this.request.body.body
+  }
+  var id = posts.push(post) - 1;
+  post.created_at = new Date;
+  post.id = id;
+  this.redirect('/');
+}
 
 /**
  * Expose `Routes`.
